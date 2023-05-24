5d9d8ef16d8272b7aad2c20935ba5ae09cc211e8
Philip Nys <p.nys@campus.tu-berlin.de>
2023-05-24 15:16:20 +0200

    added warp rust

---
+++ warp-rust/web/src/broadcaster/index.ts
@@ -0,0 +1,5 @@
+export default class Broadcaster {
+	constructor() {
+		// TODO
+	}
+}
---
+++ warp-rust/web/src/transport/message.ts
@@ -0,0 +1,7 @@
+// TODO fill in required fields
+export type Init = any
+export type Segment = any
+
+export interface Debug {
+	max_bitrate: number
+}
